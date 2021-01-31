d2ae9a6708adc5494cdfaf2fedaeca109599f038
Federico Sangati <federico.sangati@gmail.com>
2021-01-31 18:55:49 +0900

    adding random pairing seed

--- dol/run_from_dir.py
+++ dol/run_from_dir.py
@@ -17,6 +17,8 @@ def run_simulation_from_dir(**kwargs):
     genotype_idx = kwargs['genotype_idx']
     sim_index = kwargs['select_sim'] - 1
     write_data = kwargs['write_data']
+    random_target_seed = kwargs['random_target_seed']
+    random_pairing_seed = kwargs['random_pairing_seed']
 
     from pyevolver.evolution import Evolution
     evo_files = [f for f in os.listdir(dir) if f.startswith('evo_')]
@@ -28,16 +30,19 @@ def run_simulation_from_dir(**kwargs):
     sim = Simulation.load_from_file(sim_json_filepath)
     evo = Evolution.load_from_file(evo_json_filepath, folder_path=dir)
 
-    random_target_seed = kwargs['random_target_seed']
-    if random_target_seed is not None:
-        print("Setting random target")
-        sim.init_target(RandomState(random_target_seed))
-
     data_record_list = []
     # get the indexes of the populations as they were before being sorted by performance
     genotype_idx_unsorted = evo.population_sorted_indexes[genotype_idx]
     random_seed = evo.pop_eval_random_seed
-    
+
+    # overwriting simulaiton
+    if random_target_seed is not None:
+        print("Setting random target with seed ", random_target_seed)
+        sim.init_target(RandomState(random_target_seed))
+    if random_pairing_seed is not None:
+        print("Setting random pairing with seed ", random_pairing_seed)
+        random_seed =random_pairing_seed
+
     performance = sim.run_simulation(
         evo.population_unsorted, 
         genotype_idx_unsorted, 
@@ -79,7 +84,8 @@ if __name__ == "__main__":
     parser.add_argument('--dir', type=str, help='Directory path')    
     parser.add_argument('--generation', type=int, help='Number of generation to load')
     parser.add_argument('--genotype_idx', type=int, default=0, help='Index of agent in population to load')    
-    parser.add_argument('--random_target_seed', type=int, help='Index of agent in population to load')    
+    parser.add_argument('--random_target_seed', type=int, help='Seed if you want to rerun simulation with random target')    
+    parser.add_argument('--random_pairing_seed', type=int, help='Seed if you want to rerun simulation with random pairing')    
     parser.add_argument('--write_data', action='store_true', help='Whether to output data (same directory as input)')
     parser.add_argument('--select_sim', type=int, default=1, help='Which simulation to select for visualization and plot')
     parser.add_argument('--visualize_trial', type=int, default=-1, help='Whether to visualize a certain trial')
