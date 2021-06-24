5030d20c6a12a693a2f7830e188c0b362b79be59
Federico <federico.sangati@gmail.com>
2021-06-24 22:16:08 +0900

    added convergence notes in simulations

--- dol/data_path_utils.py
+++ dol/data_path_utils.py
@@ -5,30 +5,30 @@ import os
 phil_trans_si_data = os.path.join('data', 'phil_trans_si')
 
 phil_trans_si_data_overlap = {
-    'group': os.path.join(
+    'group': os.path.join( # 8 seeds converged: [1, 5, 8, 9, 12, 14, 15, 19]
         phil_trans_si_data, 
         '1d_2n_exc-0.1_zfill_rp-3_switch'
     ),
     'joint': os.path.join(
-        phil_trans_si_data, 
+        phil_trans_si_data, # 1 seed converged: [6]
         '1d_2n_exc-0.1_zfill_rp-0_switch'
     ),
     'individual': os.path.join(
-        phil_trans_si_data, 
+        phil_trans_si_data, # 0 seeds converged: []
         '1d_2n_exc-0.1_zfill_rp-3_np-4_switch'
     )
 }
 
 phil_trans_si_data_overlap = {
-    'group': os.path.join(
+    'group': os.path.join( # 20/20 seeds converged
         phil_trans_si_data, 
         '1d_2n_zfill_rp-3_overlap'
     ),
-    'joint': os.path.join(
+    'joint': os.path.join( # 20/20 seeds converged
         phil_trans_si_data, 
         '1d_2n_zfill_rp-0_overlap'
     ),
-    'individual': os.path.join(
+    'individual': os.path.join( # 20/20 seeds converged
         phil_trans_si_data, 
         '1d_2n_zfill_rp-3_np-4_overlap'
     )
