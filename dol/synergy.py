2c8ef39fa30398d980df6e8e13cee394a0772435
Federico <federico.sangati@gmail.com>
2021-07-13 12:56:06 +0900

    synergy refactoring

--- dol/synergy.py
+++ dol/synergy.py
@@ -19,13 +19,22 @@ from dol.run_from_dir import run_simulation_from_dir
 
 class InfoAnalysis:
 
+	NORM_LABELS = {
+		0: '',
+		1: 'Z-Scored',
+		2: '[0 ..1] Scaled'
+	}
+
 	def __init__(self, agent_nodes, sim_type_path, whichNormalization, max_num_seeds=None):
 		self.initiJVM()
 
 		self.agent_nodes = agent_nodes
 		self.sim_type_path = sim_type_path	
 		self.simulation_types = sim_type_path.keys()
+		
 		self.whichNormalization = whichNormalization   ## 0 : Use Orginal Data   1 : Z-Score Normalization   2 : [0 .. 1] Scaling			
+		self.norm_label = InfoAnalysis.NORM_LABELS[whichNormalization]
+
 		self.max_num_seeds = max_num_seeds # restrict it to first n seeds (for test purpose)
 
 		# self.lillieforsPValue = 0.05
@@ -70,38 +79,6 @@ class InfoAnalysis:
 		result = multiVarMICalc.computeAverageLocalOfObservations()
 
 		return result
-
-	def compute_seed_trial_means(self, info_results):		
-		simData = []
-		meanDist = []
-		stdDist = []
-
-		# num_seeds = len(info_results)
-
-		for seed_dir, seed_info_results in info_results.items():
-			seed_data = []
-			seed_tracker_mean = []
-			seed_tracker_std = []
-			for seed_info_results_trial in seed_info_results:
-				(condMultVarMI, multVarMI, coinformation, trackerTargetDist) = seed_info_results_trial
-				seed_data.append([condMultVarMI, multVarMI, coinformation])
-				seed_tracker_mean.append(trackerTargetDist.mean())
-				seed_tracker_std.append(trackerTargetDist.std())
-			
-			seed_means = np.array(seed_data).mean(axis = 0).tolist() # average across trials of (condMultVarMI, multVarMI, coinformation)
-			meanDist.append(np.mean(seed_tracker_mean)) # average across trials of seed_tracker_mean
-			stdDist.append(np.mean(seed_tracker_std)) # average across trials of seed_tracker_std
-			
-			simData.append(seed_means)
-		
-		simData = np.array(simData)
-
-		assert len(simData) == len(meanDist) == len(stdDist)
-
-		print(simData, '   ', simData.shape, '   ', len(meanDist), '  ', len(stdDist))
-		
-		return simData, meanDist, stdDist
-
 
 	def normalizeData(self, M):
 		if self.whichNormalization == 0:
@@ -151,8 +128,9 @@ class InfoAnalysis:
 			elif effectSize >= 0.5:
 				return 'Large Effect'				
 
-	def performKruskalWallis_n_PosthocWilcoxonTest(self, M, whichData, ylabel):
+	def performKruskalWallis_n_PosthocWilcoxonTest(self, M, whichData):
 		print('\n====================================',  whichData, '\n')
+		ylabel = f'{self.norm_label} {whichData}'
 		self.plotBoxPlotList(M, self.simulation_types, whichData, ylabel)
 		# sys.exit()
 		# print(M[:, 0].shape, M[:, 1].shape, M[:, 2].shape)
@@ -177,10 +155,10 @@ class InfoAnalysis:
 		print('M-' + whichOne, ' = ', np.mean(data), ' SD-' + whichOne, ' = ', np.std(data), '  Mdn-' + whichOne, ' = ', np.median(data), \
 			'  CI_95%-' + whichOne + ' = ', [np.percentile(data, 2.5), np.percentile(data, 97.5)])
 
-	def computeSpearmanCorr(self, M, distance, whichScenario, whichScaling, yLabel):
+	def computeSpearmanCorr(self, M, distance, whichScenario, ylabel):
 		fig = plt.figure(figsize = (10, 6))
-		if whichScaling != 0:
-			if whichScaling == 1:
+		if self.whichNormalization != 0:
+			if self.whichNormalization == 1:
 				meanDistGroup = [(val - np.mean(meanDistGroup))/np.std(meanDistGroup) for val in meanDistGroup]
 			else:
 				meanDistGroup = [(val - min(meanDistGroup))/(max(meanDistGroup) - min(meanDistGroup)) for val in meanDistGroup]			
@@ -194,7 +172,7 @@ class InfoAnalysis:
 			ax1.plot(M[:, i], b + m * M[:, i], 'k-')
 			# ax1.set_title(whichScenario + ' : ' + whichMeasure[i])				
 			ax1.set_xlabel(whichScenario + ' : ' + whichMeasure[i])
-			ax1.set_ylabel(yLabel)
+			ax1.set_ylabel(ylabel)
 			# plt.xticks(fontsize = 15)
 			# plt.yticks(fontsize = 15)				
 
@@ -208,7 +186,7 @@ class InfoAnalysis:
 					print(whichMeasure[i], ' vs. Target-Tracker-Distance: r = ', r, '  p-value = ', p, ' (Non-significant)')
 		plt.show()
 
-	def plotBoxPlotList(self, data, labels, ttle, yLabel):
+	def plotBoxPlotList(self, data, labels, ttle, ylabel):
 		np.random.seed(1) # same seed_dir to have same jitter (distribution of points along x axis)
 		plt.figure(figsize = (10, 6))
 		sb.boxplot(data = data, showmeans = True,
@@ -224,7 +202,7 @@ class InfoAnalysis:
 		# plt.xticks(fontsize = 30)
 		# plt.yticks(fontsize = 25)
 		plt.title(ttle)
-		plt.ylabel(yLabel)
+		plt.ylabel(ylabel)
 		plt.show()						
 
 
@@ -246,17 +224,12 @@ class InfoAnalysis:
 
 		plt.show()				
 
-	def computeDistanceMetricsForSpecificSeed(self, whichSetting, whichSeed, trial_idx, normalizationFlag, whichDistance):
+	def computeDistanceMetricsForSpecificSeed(self, whichSetting, whichSeed, trial_idx, whichDistance):
 		if not whichSeed in list(set(os.listdir(self.sim_type_path[whichSetting]))):
 			print(whichSeed, '  Is Not a Valid Seed')				
 			sys.exit()
 
-		dir = os.path.join(self.sim_type_path[whichSetting], whichSeed)
-		perf, sim_perfs, evo, sim, data_record_list, sim_idx = run_simulation_from_dir(dir)			
-
-		simIndex = sim_perfs.index(min(sim_perfs))	  ### sim_perf is normalized, therefore, using 'minimum distance'			
-		
-		data = data_record_list[simIndex]
+		data = self.data[whichSetting][whichSeed]
 
 		agent1 = np.concatenate([data[node][trial_idx,0,:,:] for node in self.agent_nodes], axis=1)
 		agent2 = np.concatenate([data[node][trial_idx,1,:,:] for node in self.agent_nodes], axis=1)
@@ -283,7 +256,7 @@ class InfoAnalysis:
 			f'{whichSetting} {whichSeed} Trial {trial_idx+1} {whichDistance} Distance'
 		)
 
-	def read_data(self):
+	def build_data(self):
 		
 		self.data = {} # dictionary sim_type -> seed_dir -> sim_data
 		
@@ -310,6 +283,13 @@ class InfoAnalysis:
 				sim_data = data_record_list[simIndex]
 
 				self.data[sim_type][seed_dir] = sim_data
+		
+		self.init_data_info()
+
+	def init_data_info(self):
+		first_seed_sim_data = next(iter(self.data.values()))
+		first_sim_data = next(iter(first_seed_sim_data.values()))
+		self.num_trials, self.num_data_points = first_sim_data['delta_tracker_target'].shape
 
 
 	def save_data_to_pickle(self, pickle_file):
@@ -319,56 +299,62 @@ class InfoAnalysis:
 
 	def load_data_from_pickle(self, pickle_file):
 		with open(pickle_file, 'rb') as handle:
-			self.data = pickle.load(handle)								
+			self.data = pickle.load(handle)		
+		self.init_data_info()						
 
 
 	def compute_synergy(self):
 
-		results = {}
+		results = {} 
+		# overall results sim_type -> sim_type_result
+		# where sym_type_result is a dictionary: measure -> data (num_seed)
+		nodes_measures = ['condMultVarMI', 'multVarMI', 'coinformation']
+		dist_measures = ['trackerTargetDistMean', 'trackerTargetDistStd']
+		results_measures = nodes_measures + dist_measures
 		
 		for sim_type, seed_sim_data in self.data.items():
+			
+			num_seeds = len(seed_sim_data)			
 
-			info_results = {} 
+			sim_type_results = {
+				measure: np.zeros((num_seeds, self.num_trials))
+				for measure in results_measures
+			}
 
-			for seed_dir, sim_data in seed_sim_data.items():
-				
-				seed_info_results = info_results[seed_dir] = []
+			for s, sim_data in enumerate(seed_sim_data.values()):
 
-				num_trials = len(sim_data['trials_performances'])
-				
-				for trialIndex in range(num_trials):
-					# print('Trial # ', (trialIndex + 1))
-					# for each trial we save the following tuple: (condMultVarMI, multVarMI, coinformation, trackerTargetDist)
-					agent1 = np.concatenate([sim_data[node][trialIndex,0,:,:] for node in self.agent_nodes], axis=1)
-					agent2 = np.concatenate([sim_data[node][trialIndex,1,:,:] for node in self.agent_nodes], axis=1)
-					target_pos = sim_data['target_position'][trialIndex]
-					condMultVarMI = self.computeConditionalMultiVariateMutualInfo(agent1, agent2, np.expand_dims(target_pos, axis = 0).T)
-					multVarMI = self.computeMultiVariateMutualInfo(agent1, agent2)
-					coinformation = condMultVarMI - multVarMI  #### a.k.a interaction information, net synergy, and integration			
-					trackerTargetDist = sim_data['delta_tracker_target'][trialIndex]												
+				# s is the seed index
+				# sim_data is the dictionary with the results from the simulation
 
-					seed_info_results.append(
-						(condMultVarMI, multVarMI, coinformation, trackerTargetDist)
-					)
+				delta_tracker_target = sim_data['delta_tracker_target'] # (num_trials, num_data_points)
 
-			results[sim_type] = self.compute_seed_trial_means(info_results) # cond_mult_coinfo_mean, meanDist, stdDist
+				for t in range(self.num_trials):
+					# print('Trial # ', (t + 1))
+					agent1 = np.concatenate([sim_data[node][t,0,:,:] for node in self.agent_nodes], axis=1)
+					agent2 = np.concatenate([sim_data[node][t,1,:,:] for node in self.agent_nodes], axis=1)
+					target_pos = sim_data['target_position'][t]
+					sim_type_results['condMultVarMI'][s,t] = condMultVarMI = self.computeConditionalMultiVariateMutualInfo(agent1, agent2, np.expand_dims(target_pos, axis = 0).T)
+					sim_type_results['multVarMI'][s,t] = multVarMI = self.computeMultiVariateMutualInfo(agent1, agent2)
+					sim_type_results['coinformation'][s,t] = condMultVarMI - multVarMI  #### a.k.a interaction information, net synergy, and integration														
+					sim_type_results['trackerTargetDistMean'][s,t] = delta_tracker_target[t].mean()
+					sim_type_results['trackerTargetDistStd'][s,t] = delta_tracker_target[t].std()
+					
+			# compute mean across trials
+			# all variables will be 1-dim array with num_seeds elements
+			for measure in sim_type_results:
+				sim_type_results[measure] = sim_type_results[measure].mean(axis=1)
+
+			results[sim_type] = sim_type_results
 
 
-		condMultVarMI = np.array([results[sim_type][0][:,0] for sim_type in self.simulation_types]).T
-		multVarMI = np.array([results[sim_type][0][:,1] for sim_type in self.simulation_types]).T
-		coinformation = np.array([results[sim_type][0][:,2] for sim_type in self.simulation_types]).T
+		condMultVarMI = np.array([results[sim_type]['condMultVarMI'] for sim_type in self.simulation_types]).T
+		multVarMI = np.array([results[sim_type]['multVarMI'] for sim_type in self.simulation_types]).T
+		coinformation = np.array([results[sim_type]['coinformation'] for sim_type in self.simulation_types]).T
 
 		condMultVarMI = self.normalizeData(condMultVarMI) 
 		multVarMI = self.normalizeData(multVarMI)
 		coinformation = self.normalizeData(coinformation)
 
-
-		if self.whichNormalization == 0:
-			yLabel = ''			
-		elif self.whichNormalization == 1:
-			yLabel = 'Z-Scored '
-		elif self.whichNormalization == 2:
-			yLabel = '[0 ..1] Scaled '
 
 		################# We might want to check whether data follows normal distribution and if positive apply parametric tests instead.
 
@@ -376,31 +362,29 @@ class InfoAnalysis:
 		# self.checkDataNormality(multVarMI.flatten().tolist(), 'Multivariate Mutual Information')
 		# self.checkDataNormality(coinformation.flatten().tolist(), 'Net-Synergy')			
 
-		self.performKruskalWallis_n_PosthocWilcoxonTest(condMultVarMI, 'Multivariate Conditional Mutual Information', yLabel + 'Multivariate Conditional Mutual Information')
-		self.performKruskalWallis_n_PosthocWilcoxonTest(multVarMI, 'Multivariate Mutual Information', yLabel + 'Multivariate Mutual Information')
-		self.performKruskalWallis_n_PosthocWilcoxonTest(coinformation, 'Net-Synergy', yLabel + 'Net-Synergy')		
+		self.performKruskalWallis_n_PosthocWilcoxonTest(condMultVarMI, f'Multivariate Conditional Mutual Information')
+		self.performKruskalWallis_n_PosthocWilcoxonTest(multVarMI, f'Multivariate Mutual Information')
+		self.performKruskalWallis_n_PosthocWilcoxonTest(coinformation, f'Net-Synergy')		
 
 		print('\n\n Spearman Correlation Based on Target-Tracker Mean Distance')
 
-		for sim_type in self.simulation_types:
-			cond_mult_coinfo_mean, meanDist, stdDist = results[sim_type]
+		for sim_type, sim_type_results in results.items():
+			cond_mult_coinfo_mean = np.array([sim_type_results[m] for m in nodes_measures]).T
 			self.computeSpearmanCorr(
 				cond_mult_coinfo_mean, 
-				meanDist, 
+				sim_type_results['trackerTargetDistMean'], 
 				sim_type, 
-				self.whichNormalization, 
 				'Mean Target-Tracker Disatnce'
 			)  ##### 1 : z-scored   2 : [0 .. 1] scaled
 
 		print('\n\n Spearman Correlation Based on Target-Tracker SD Distance')
 
-		for sim_type in self.simulation_types:
-			cond_mult_coinfo_mean, meanDist, stdDist = results[sim_type]
+		for sim_type, sim_type_results in results.items():
+			cond_mult_coinfo_mean = np.array([sim_type_results[m] for m in nodes_measures]).T
 			self.computeSpearmanCorr(
 				cond_mult_coinfo_mean, 
-				stdDist, 
+				sim_type_results['trackerTargetDistStd'], 
 				sim_type, 
-				self.whichNormalization, 
 				'SD Target-Tracker Disatnce'
 			)		
 
@@ -423,31 +407,30 @@ if __name__ == "__main__":
 	pickle_path = 'results/synergy.pickle' # where data is saved/loaded
 	
 	load_data = True # set to True if data is read from pickle (has to be saved beforehand)
-	save_data = True # set to True if data will be saved to pickle (to be loaded faster successively)
+	save_data = False # set to True if data will be saved to pickle (to be loaded faster successively)
 	
 	IA = InfoAnalysis(
 		agent_nodes = agent_nodes, 
 		sim_type_path = overlap_data_dirs,
 		whichNormalization = 0,   ## 0 : Use Orginal Data   1 : Z-Score Normalization   2 : [0 .. 1] Scaling			
-		max_num_seeds = None # set to low number to test few seeds, set to None to compute all seeds
+		max_num_seeds = 5 # set to low number to test few seeds, set to None to compute all seeds
 	)
 	
-	if load_data:
-		assert os.path.exists(pickle_path), f'File {pickle_path} does not exist'
+	if load_data and os.path.exists(pickle_path):
 		IA.load_data_from_pickle(pickle_path)
 	else:
-		IA.read_data()
+		IA.build_data()
 	
 	if save_data:
 		IA.save_data_to_pickle(pickle_path)
 	
-	IA.compute_synergy()
+	# IA.compute_synergy()
 
 	''' 
 	correlation = 1 - corr(x, y)  AND  canberra = \sum_i (abs(x_i - y_i))/(abs(x_i) + abs(y_i))
 	'''
 	distanceMetrics = ['cosine', 'correlation', 'euclidean', 'cityblock', 'canberra']   
 	for metric in distanceMetrics:
-		IA.computeDistanceMetricsForSpecificSeed('individual', 'seed_010', 1, 0, metric)
+		IA.computeDistanceMetricsForSpecificSeed('individual', 'seed_001', 0, metric)
 
 	IA.shutdownJVM()						
