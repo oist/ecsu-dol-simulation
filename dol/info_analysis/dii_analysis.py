5279ef05eaa3803a4d014eb4bb2aa000f83a0575
Federico <federico.sangati@gmail.com>
2021-12-07 23:08:38 +0900

    dii SpearmanCorr (delta tracker target)

--- dol/info_analysis/dii_analysis.py
+++ dol/info_analysis/dii_analysis.py
@@ -14,70 +14,6 @@ from scipy.stats import spearmanr, ranksums
 
 
 
-def load_data_from_pickle(pickle_file):
-    with open(pickle_file, 'rb') as handle:
-        data = pickle.load(handle)		
-    return data
-
-def compute_info_values(sim_data, agent_nodes, conditioning_node, powerset=False):
-    """compute info measures for data of a specific simulation type and a specific seed
-
-    Args:
-        sim_data ([type]): [description]
-        agent_nodes ([type]): [description]
-        conditioning_node ([type]): [description]
-
-    Returns:
-        [type]: [description]
-    """
-    
-    # we know num trials is first dim in data
-    # TODO: fix this to make it more robust
-    num_trials = sim_data[agent_nodes[0]].shape[0]
-
-    mi_trials = np.zeros(num_trials)
-    mi_overall_trials = np.zeros(num_trials)
-    cond_mi_trials = np.zeros(num_trials)
-    cond_mi_overall_trials = np.zeros(num_trials)
-    synergy_overall = np.zeros(num_trials)
-    if powerset:
-        synergy_powerset = np.zeros(num_trials)
-        
-    for t in range(num_trials):
-        agent1 = np.concatenate([sim_data[node][t,0,:,:] for node in agent_nodes], axis=1)
-        agent2 = np.concatenate([sim_data[node][t,1,:,:] for node in agent_nodes], axis=1)
-        target = sim_data[conditioning_node][t]
-        dii = DII(agent1, agent2, target)        
-        overall_mi, overall_cond_mi = dii.compute_dii_overall()        
-        if powerset:
-            mi_matrix, cond_mi_matrix = dii.compute_dii_powerset() 
-            mi_trials[t] = mi_matrix.mean()                
-            cond_mi_trials[t] = cond_mi_matrix.mean()
-            synergy_powerset[t] = cond_mi_trials[t] - mi_trials[t]
-        
-        cond_mi_overall_trials[t] = overall_cond_mi        
-        mi_overall_trials[t] = overall_mi
-        synergy_overrall_trial = cond_mi_overall_trials[t] - mi_overall_trials[t]
-        synergy_overall[t] = synergy_overrall_trial
-
-    result = {
-        'MI overall': mi_overall_trials.mean(),
-        'CMI overall': cond_mi_overall_trials.mean(),
-        'Synergy overall': synergy_overall.mean()
-    }
-    if powerset:
-        result.update(
-            {
-                'MI powerset': mi_trials.mean(),
-                'Cond MI powerset': cond_mi_trials.mean(),
-                'Synergy powerset': synergy_powerset.mean()
-            }
-        )
-
-    return result
-
-
-
 def perform_analysis(IA, agent_nodes, conditioning_node, powerset=False):
 
     data = IA.data
@@ -110,6 +46,7 @@ def perform_analysis(IA, agent_nodes, conditioning_node, powerset=False):
     
     result_metrics = list(sim_type_results[first_sim_type][0].keys())
     '''
+    conditioning_node
     'MI powerset'
     'Cond MI powerset'
     'MI overall'
@@ -118,15 +55,22 @@ def perform_analysis(IA, agent_nodes, conditioning_node, powerset=False):
     'Synergy overall'
     '''
 
-    
     # # SpearmanCorr for ['MI overall', 'CMI overall', 'Synergy overall']        
-    '''
     for x in ['MI overall', 'CMI overall', 'Synergy overall']:            
-        delta = None # average target-tracker distance for all seeds (averaged across timesteps and then trials)
-        # self.delta_target = target_position - self.position
-        # delta_abs = np.abs(self.delta_target)
-        [r, p] = spearmanr(x, delta) # where x and delta are arrays of num_seeds values
-    '''
+        for sim_type in sim_types:
+            target = [ # average target-tracker distance for all seeds (averaged across timesteps and then trials)        
+                sim_type_results[sim_type][s][conditioning_node] 
+                for s in range(num_seeds)
+            ]            
+            measure_values = [
+                sim_type_results[sim_type][s][x] 
+                for s in range(num_seeds)
+            ]
+            [r, p] = spearmanr(measure_values, target) # where x and delta are arrays of num_seeds values            
+            print('-------')
+            print(f'SpearmanCorr {sim_type} - {x} - {conditioning_node}')
+            print('r:', r)
+            print('p:', p)
     
     for metric in result_metrics:
 
@@ -148,7 +92,7 @@ def perform_analysis(IA, agent_nodes, conditioning_node, powerset=False):
             ylabel = metric,
             output_file = output_file
         )
-
+        
         print('-------------')
         print(metric)
         print(f'sim_types: {sim_types}')
@@ -157,8 +101,69 @@ def perform_analysis(IA, agent_nodes, conditioning_node, powerset=False):
         print(sim_type[0], ' vs. ', sim_type[1], '  s = ', sW, '  p = ', pW, '  effect-size = ', effectSize, '(', \
             interpret_observed_effect_size(effectSize, 2), ')')
         show_descriptive_stats(sims_metric_data[0], sim_type[0])
-        show_descriptive_stats(sims_metric_data[1], sim_type[1])
+        show_descriptive_stats(sims_metric_data[1], sim_type[1])        
+
+
+def compute_info_values(sim_data, agent_nodes, conditioning_node, powerset=False):
+    """compute info measures for data of a specific simulation type and a specific seed
+
+    Args:
+        sim_data ([type]): [description]
+        agent_nodes ([type]): [description]
+        conditioning_node ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    
+    # we know num trials is first dim in data
+    # TODO: fix this to make it more robust
+    num_trials = sim_data[agent_nodes[0]].shape[0]
+
+    mi_trials = np.zeros(num_trials)
+    mi_overall_trials = np.zeros(num_trials)
+    cond_mi_trials = np.zeros(num_trials)
+    cond_mi_overall_trials = np.zeros(num_trials)
+    synergy_overall = np.zeros(num_trials)
+    target_trials = np.zeros(num_trials)
+    if powerset:
+        synergy_powerset = np.zeros(num_trials)
         
+    for t in range(num_trials):
+        agent1 = np.concatenate([sim_data[node][t,0,:,:] for node in agent_nodes], axis=1)
+        agent2 = np.concatenate([sim_data[node][t,1,:,:] for node in agent_nodes], axis=1)
+        target = sim_data[conditioning_node][t]
+        target_trials[t] = target.mean() # average across time steps
+        dii = DII(agent1, agent2, target)        
+        overall_mi, overall_cond_mi = dii.compute_dii_overall()        
+        if powerset:
+            mi_matrix, cond_mi_matrix = dii.compute_dii_powerset() 
+            mi_trials[t] = mi_matrix.mean()                
+            cond_mi_trials[t] = cond_mi_matrix.mean()
+            synergy_powerset[t] = cond_mi_trials[t] - mi_trials[t]
+        
+        cond_mi_overall_trials[t] = overall_cond_mi        
+        mi_overall_trials[t] = overall_mi
+        synergy_overrall_trial = cond_mi_overall_trials[t] - mi_overall_trials[t]
+        synergy_overall[t] = synergy_overrall_trial
+
+    result = {
+        conditioning_node: target_trials.mean(),
+        'MI overall': mi_overall_trials.mean(),
+        'CMI overall': cond_mi_overall_trials.mean(),
+        'Synergy overall': synergy_overall.mean()
+    }
+    if powerset:
+        result.update(
+            {
+                'MI powerset': mi_trials.mean(),
+                'Cond MI powerset': cond_mi_trials.mean(),
+                'Synergy powerset': synergy_powerset.mean()
+            }
+        )
+
+    return result
+
 
 if __name__ == "__main__":
 
@@ -171,26 +176,3 @@ if __name__ == "__main__":
     IA = build_info_analysis_from_experiments()
     perform_analysis(IA, agent_nodes, conditioning_node, powerset=False)
     
-    # import argparse
-
-    # parser = argparse.ArgumentParser(
-    #     description='DII Simulation Analysis'
-    # )
-
-    # parser.add_argument('--pickle_path', type=str, required=True, help='Pickle path')
-    # parser.add_argument('--num_cores', type=int, default=1, help='Number of cores to used (defaults to 1)')
-    # parser.add_argument('--powerset', action='store_true', default=False, help='Whether to compute powerset metrics')
-    # parser.add_argument('--output_dir', type=str, default=None, help='Output dir where to save plots (defaults to None: disply plots to screen)')
-
-    # args = parser.parse_args()
-
-    # pickle_path = args.pickle_path 
-    # # pickle_path = '/Users/fedja/Code/ECSU/Evolution/dol-simulation/results/alife21_5seeds.pickle'
-    # # args.num_cores = 5
-    # # args.output_dir = './results/alife21_5'
-    # data = load_data_from_pickle(pickle_path)
-
-    # if args.output_dir is not None and not os.path.exists(args.output_dir):
-    #     os.makedirs(args.output_dir)
-
-    # perform_analysis(data, args.output_dir, args.num_cores, agent_nodes, conditioning_node, args.powerset)
